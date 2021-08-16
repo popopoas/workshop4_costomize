7ca429f248ae7df9b436b340964064eb73e65095
popopoas <mux141259@gmail.com>
2021-08-16 12:07:44 +0700

    first commit

---
+++ src/main/java/com/fleid/workshop3_costomize/Workshop3CostomizeApplication.java
@@ -0,0 +1,13 @@
+package com.fleid.workshop3_costomize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop3CostomizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop3CostomizeApplication.class, args);
+	}
+
+}
